c92f1e20cbf93f31ed2b1bed4c1dcbc4c5e67460
FoxBitExe <52263424+FoxBitExe@users.noreply.github.com>
2019-07-11 02:04:41 +0700

    Update program.ino

--- program.ino
+++ program.ino
@@ -1,3 +1,22 @@
+void setup() 
+{
+  Serial.begin(115200);
+  WifiStatus();
+
+  bot.sendMessage(chatid, "Bot Terkoneksi");
+  Serial.print("\n\nBOT Sudah Siap Di Gunakan .....");
+  
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+
+  pinMode(RELAY,OUTPUT);
+  digitalWrite(RELAY,HIGH);
+  
+  Serial.println("Tempelkan RFID tag ...");
+  Serial.println();
+
+}
+
 void loop() 
 {
   // Look for new cards
@@ -49,4 +68,23 @@ void loop()
     bot.sendMessage(chatid, "Access denied  " + content.substring(1));
     Serial.println("Notif Sended To Telegram\n");
   }
+}
+
+void WifiStatus() {
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  Serial.print("Connecting Wifi: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
